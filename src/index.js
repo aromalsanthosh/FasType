2f21690990224ed0c915003719c3a26d0464cae0
Aromal S <aromalsanthosh@protonmail.com>
2021-06-12 13:17:56 +0530

    Added-App-Component🌐

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import App from './components/App/App';
 
 
 ReactDOM.render(
@@ -10,5 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
