30f6bf7fd18de788596ba0a2d42da9f4cfc8fd1e
Aromal S <aromalsanthosh@protonmail.com>
2021-06-27 11:51:28 +0530

    Test-Logic-Sucess💯

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ import ChallengeSection from '../ChallengeSection/ChallengeSection';
 
 const TotalTime =60;
 
-//const ServiceUrl = "http://metaphorpsum.com/paragraphs/1/9";
+const ServiceUrl = "http://metaphorpsum.com/paragraphs/1/9";
 
 
 class App extends React.Component{
@@ -23,29 +23,25 @@ class App extends React.Component{
     }
 
     componentDidMount() {
-        // fetch(ServiceUrl)
-        //     .then(response => response.text())
-        //     .then(data =>{
-        //         console.log("APi response is ",data);
-        //         this.setState({selectedParagraph: data});
+        fetch(ServiceUrl)
+            .then(response => response.text())
+            .then(data =>{
+                console.log("APi response is ",data);
+                
+                const selectedParagraphArray= data.split("");
+                console.log("Splitted Array-",selectedParagraphArray);
 
-        //     });
+                const testInfo = selectedParagraphArray.map(selectedLetter =>{
+                    return{
+                        testLetter: selectedLetter,
+                        status: "notAttempted",
+                    };
+                });
 
-        const selectedParagraphArray= this.state.selectedParagraph.split("");
-        console.log("Splitted Array-",selectedParagraphArray);
+                this.setState({testInfo,selectedParagraph: data});
 
-        const testInfo = selectedParagraphArray.map(selectedLetter =>{
-            return{
-                testLetter: selectedLetter,
-                status: "notAttempted",
-            }
-        });
+            });
 
-        this.setState({testInfo: testInfo})
-
-
-
-        
     }
 
     startTimer=()=>{
@@ -71,8 +67,69 @@ class App extends React.Component{
     }
 
     handleUserInput = (inputValue) => {
-        if(!this.state.timerStarted) this.startTimer();
-        //console.log(inputValue);
+        if (!this.state.timerStarted) this.startTimer();
+
+        /**
+         * 1. Handle the underflow case - all characters should be shown as not-attempted
+         * 2. Handle the overflow case - early exit
+         * 3. Handle the backspace case
+         *      - Mark the [index+1] element as notAttempted
+         *        (irrespective of whether the index is less than zero)
+         *      - But, don't forget to check for the overflow here
+         *        (index + 1 -> out of bound, when index === length-1)
+         * 4. Update the status in test info
+         *      1. Find out the last character in the inputValue and it's index
+         *      2. Check if the character at same index in testInfo (state) matches
+         *      3. Yes -> Correct
+         *         No  -> Incorrect (Mistake++)
+         * 5. Irrespective of the case, characters, words and wpm can be updated
+         */
+
+        const characters = inputValue.length;
+        const words = inputValue.split(" ").length;
+        const index = characters - 1;
+
+        if (index < 0) {
+            this.setState({
+                testInfo: [
+                    {
+                        testLetter: this.state.testInfo[0].testLetter,
+                        status: "notAttempted",
+                    },
+                    ...this.state.testInfo.slice(1),
+                ],
+                characters,
+                words,
+            });
+
+            return;
+        }
+
+        if (index >= this.state.selectedParagraph.length) {
+            this.setState({
+                characters,
+                words,
+            });
+            return;
+        }
+
+        // Make a copy
+        const testInfo = this.state.testInfo;
+        if (!(index === this.state.selectedParagraph.length - 1))
+            testInfo[index + 1].status = "notAttempted";
+
+        // Check for mistake
+        const isMistake = inputValue[index] === testInfo[index].testLetter;
+
+        // Update the testInfo
+        testInfo[index].status = isMistake ? "correct" : "incorrect";
+
+        // Update the state
+        this.setState({
+            testInfo,
+            words,
+            characters,
+        });
     };
 
 
